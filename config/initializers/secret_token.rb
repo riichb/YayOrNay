d6d9ad7f71a00a2718db96a9f5ab206c67b2633c
Seth McLaughlin <bustosrichard27@gmail.com>
2013-07-24 07:47:23 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yayornay::Application.config.secret_token = '678be327858050b1e25d2d487b748b94791a189a472e6aa0fd2a586c8fada32125c9d152fb581696baf4ef9c49d1f63b7d95540d752d2365e4e9b4e840e23114'
